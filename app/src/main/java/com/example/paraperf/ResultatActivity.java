b99d4183bc0e388bb43d80e45a0b53a7915403bc
mariegiannoni <marie.giannoni@isen.yncrea.fr>
2020-05-11 15:07:15 +0200

    Un media player a été codé et ajouté sous la vidéo. Il est presque effectif (75%). Il faut que je trouve encore comment résoudre un bug dont la provenance n'est pas encore identifiée : quand je clique sur play pour lancer la vidéo, l'activité plante en affichant un message d'erreur et se ferme.
    Ce qu'il reste à faire :
    1. Corriger le bug de la vidéo quand on appuie sur play.
    2. Corriger la barre d'avancement qui ne permet pas encore de positionner l'avancement où l'on souhaite.
    3. Appel des fonctions de création du graphique et du fichier Excel généré (détection des pics)
    4. Chargement d'un graphique à partir de la lecture d'un fichier Excel.
    5. Synchronisation de la vidéo et du graphique.
    6. Possibilité de télécharger le fichier Excel résultant des calculs en appuyant sur un bouton.

--- app/src/main/java/com/example/paraperf/ResultatActivity.java
+++ app/src/main/java/com/example/paraperf/ResultatActivity.java
@@ -5,11 +5,16 @@ import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -43,6 +48,19 @@ public class ResultatActivity extends AppCompatActivity {
     private String cheminDonnees;
     private String cheminVideo;
 
+    // Boutons play, pause, avance, rembobiner, debut
+    private ImageButton boutonPlayPause;
+    private ImageButton boutonAvance;
+    private ImageButton boutonRembobine;
+    private ImageButton boutonDebut;
+
+    // Temps de gestion de la vidéo
+    private Handler handlerVideo = new Handler();
+    private double currentTime = 0;
+    private double startTime = 0;
+    private double finalTime = 0;
+    private int avanceTime = 1000; //ms
+    private int rembobineTime = 1000; //ms
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +73,10 @@ public class ResultatActivity extends AppCompatActivity {
         titreResultat =  (TextView) findViewById(R.id.titreResultat);
         titreVideo =  (TextView) findViewById(R.id.titreVideo);
         titreGraphique =  (TextView) findViewById(R.id.titreGraphique);
+        boutonPlayPause = (ImageButton) findViewById(R.id.play_pause);
+        boutonAvance = (ImageButton) findViewById(R.id.avance);
+        boutonRembobine = (ImageButton) findViewById(R.id.rembobiner);
+        boutonDebut = (ImageButton) findViewById(R.id.debut);
 
         init();
     }
@@ -66,13 +88,30 @@ public class ResultatActivity extends AppCompatActivity {
         // Affichage de la vidéo
         try {
             vueVideo.setVideoPath(cheminVideo);
-            vueVideo.start();
         } catch(Exception e) {
             Context context = getApplicationContext();
             Log.e("Error Play Local Video:", e.getMessage());
             Toast.makeText(context,"Une erreur est survenue en jouant la vidéo : "+ e.getMessage(),Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
+
+        // Récupération du temps de la vidéo
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(cheminVideo);
+        String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        finalTime = Double.parseDouble(time);
+        startTime = vueVideo.getCurrentPosition();
+        currentTime = vueVideo.getCurrentPosition();
+
+        Log.d("Time", finalTime + " " + startTime + " " + currentTime);
+
+        // Initialisation du curseur d'avancement
+        if (finalTime != 0) {
+            curseurAvancement.setMax((int) finalTime);
+        }
+        curseurAvancement.setProgress((int) currentTime);
+
+        handlerVideo.postDelayed(changePositionTemps, 100);
     }
 
     // Permet l'affichage du menu sur la page de résultats
@@ -100,15 +139,75 @@ public class ResultatActivity extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        vueVideo.start();
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        vueVideo.suspend();
-//    }
+    /* Gestion de la vidéo */
+    // Bouton play et pause
+    public void playPause(View v) {
+        if(vueVideo.isPlaying() && vueVideo.canPause()) {
+            vueVideo.pause();
+            Drawable drawable = getResources().getDrawable(getResources().getIdentifier("pause", "drawable", getPackageName()));
+            boutonPlayPause.setImageDrawable(drawable);
+        }
+        else if(currentTime <= finalTime){
+            vueVideo.start();
+            Drawable drawable = getResources().getDrawable(getResources().getIdentifier("start", "drawable", getPackageName()));
+            boutonPlayPause.setImageDrawable(drawable);
+        }
+        else {
+            currentTime = startTime;
+            curseurAvancement.setProgress((int) currentTime);
+            vueVideo.seekTo((int) currentTime);
+        }
+    }
+
+    // Bouton avance
+    public void avance(View v) {
+        if (vueVideo.canSeekForward()){
+            currentTime = currentTime + avanceTime;
+            if(currentTime >= finalTime) {
+                currentTime = finalTime;
+            }
+            curseurAvancement.setProgress((int) currentTime);
+            vueVideo.seekTo((int) currentTime);
+        }
+    }
+
+    // Bouton rembobine
+    public void rembobine(View v) {
+        if (vueVideo.canSeekBackward()){
+            currentTime = currentTime - rembobineTime;
+            if(currentTime <= startTime) {
+                currentTime = startTime;
+            }
+            curseurAvancement.setProgress((int) currentTime);
+            vueVideo.seekTo((int) currentTime);
+        }
+    }
+
+    // Bouton retour au début
+    public void debut(View v) {
+        if (vueVideo.canSeekBackward()){
+            currentTime = startTime;
+            curseurAvancement.setProgress((int) currentTime);
+            vueVideo.seekTo((int) currentTime);
+        }
+    }
+
+    // Barre d'avancement : positionnement
+    public void positionne(View v) {
+        currentTime = (double) curseurAvancement.getProgress();
+        if (currentTime >= startTime && currentTime <= finalTime) {
+            curseurAvancement.setProgress((int) currentTime);
+            vueVideo.seekTo((int) currentTime);
+        }
+    }
+
+    // Avancement de la barre d'avancement
+    private Runnable changePositionTemps = new Runnable() {
+        @Override
+        public void run() {
+            currentTime = vueVideo.getCurrentPosition();
+            curseurAvancement.setProgress((int) currentTime);
+            handlerVideo.postDelayed(this, 100);
+        }
+    };
 }
